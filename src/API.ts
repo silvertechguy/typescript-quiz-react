655f1f2d3e837d10665e5792b10ac5f94ab5e562
Ali Fadda <silvertechguy@gmail.com>
2020-10-03 13:43:28 +0200

    typescript quiz react

---
+++ src/API.ts
@@ -0,0 +1,17 @@
+import { Difficulty, Question, QuestionsState } from "./types";
+import { shuffleArray } from "./utils";
+
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+): Promise<QuestionsState[]> => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const data = await (await fetch(endpoint)).json();
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer,
+    ]),
+  }));
+};
